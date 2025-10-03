c76202b976c3d2b8dcca4bc14012123e629f297b
Yufeng Zhao <yufeng-zhao@outlook.com>
2025-10-02 23:39:01 -0400

    github action build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['instagram.com', 'www.instagram.com', 'cdn.itch.zone'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
